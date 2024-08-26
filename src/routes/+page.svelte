f8997e5fc522c4b3d8165f887ce98f775b8d0e94
sblbl <sblbl@Computer-portatile-di-sblbl.local>
2024-08-26 11:23:43 +0200

    pwa fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -22,7 +22,7 @@
 </script>
 
 <section
-	class="fixed top-0 left-0 w-screen h-screen bg-blue-raspberry text-light-cream flex flex-col items-center justify-center"
+	class="fixed top-0 left-0 w-screen h-dvh bg-blue-raspberry text-light-cream flex flex-col items-center justify-center"
 >
 	<img src="/favicon.png" alt="lampone" class="max-h-64" />
 	<h1 class="text-6xl text-light-cream mb-2">lampone</h1>
