f6aca933413a1e5b69205da0bcac47dda68b9cde
sblbl <sblbl@Computer-portatile-di-sblbl.local>
2024-08-26 15:59:44 +0200

    fixes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,7 +2,7 @@
 </script>
 
 <section
-	class="fixed top-0 left-0 w-screen h-screen max-h-dvh bg-blue-raspberry text-light-cream flex flex-col items-center justify-center"
+	class="fixed top-0 left-0 w-screen h-screen max-h-lvh bg-blue-raspberry text-light-cream flex flex-col items-center justify-center"
 >
 	<img src="/favicon.png" alt="lampone" class="max-h-64" />
 	<h1 class="text-6xl text-light-cream mb-2">lampone</h1>
